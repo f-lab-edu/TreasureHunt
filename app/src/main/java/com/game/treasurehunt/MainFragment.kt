430f3c7b00611f4e3db2dc13a4ca37a3d927c925
YOUSEUNGHAK <hakyouyouhi@gmail.com>
2023-07-23 23:21:41 +0900

    fix: MainFragment.kt waring 해결
    - waring : 'onCreateView' always returns non-null type
    - View? -> View로 변경
    
    #13

--- app/src/main/java/com/game/treasurehunt/MainFragment.kt
+++ app/src/main/java/com/game/treasurehunt/MainFragment.kt
@@ -15,7 +15,7 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentMainBinding.inflate(inflater)
         return binding.root
     }
