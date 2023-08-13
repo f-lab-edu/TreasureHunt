f089cf2b3fc92c481ab690ab7318f92acbfa373e
YOUSEUNGHAK <hakyouyouhi@gmail.com>
2023-08-13 21:31:58 +0900

    feat : 지도에 내 위치 표시
    1. Map 시작할 때 내 위치 부터 시작
    2. 현재 위치 버튼 누르면 내 위치로 이동
    3. 일정 간격으로 현재 위치 디버그 표시
    
    #28

--- app/src/main/java/com/game/treasurehunt/MainFragment.kt
+++ app/src/main/java/com/game/treasurehunt/MainFragment.kt
@@ -1,16 +1,57 @@
 package com.game.treasurehunt
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import com.game.treasurehunt.databinding.FragmentMainBinding
 import com.game.treasurehunt.inquiry.InquiryFragment
 import com.game.treasurehunt.treasureList.TreasureListFragment
+import com.google.android.gms.location.*
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraPosition
+import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.overlay.Marker
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(), OnMapReadyCallback {
     private lateinit var binding: FragmentMainBinding
+    private lateinit var naverMap: NaverMap
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    private val locationPermissionRequest = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        when {
+            permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                getCurrentLocation()
+            }
+            permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                getCurrentLocation()
+            }
+            else -> {
+                // TODO 설정으로 보내기 or 교육용 팝업을 띄어서 다시 권한 요청하기
+            }
+        }
+    }
+
+    private val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            // 새로 요청된 위치 정보
+            for (location in locationResult.locations) {
+                Log.d("location", "(${location.latitude}, ${location.longitude})")
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -45,7 +86,93 @@ class MainFragment : Fragment() {
                 commit()
             }
         }
+
+        binding.fabMainMyLocation.setOnClickListener {
+            moveLastLocation()
+        }
     }
 
+    override fun onResume() {
+        super.onResume()
 
+        requestLocationPermission()
+        startMapProcess()
+    }
+
+    private fun requestLocationPermission() {
+        locationPermissionRequest.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        )
+    }
+
+    private fun startMapProcess() {
+        fusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(requireContext())
+
+        val fm = childFragmentManager
+        val mapFragment = fm.findFragmentById(R.id.map_fragment) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                fm.beginTransaction().add(R.id.map_fragment, it).commit()
+            }
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(naverMap: NaverMap) {
+        val cameraPosition = CameraPosition(
+            LatLng(37.5666102, 126.9783881),  // 위치 지정
+            16.0 // 줌 레벨
+        )
+        naverMap.cameraPosition = cameraPosition
+        this.naverMap = naverMap
+
+        moveLastLocation()
+    }
+
+    private fun getCurrentLocation() {
+        val locationRequest =
+            LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 5 * 1000).build()
+
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(), Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestLocationPermission()
+            return
+        }
+
+        // 권한이 있는 상태
+        fusedLocationProviderClient.requestLocationUpdates(
+            locationRequest, locationCallback, Looper.getMainLooper()
+        )
+
+        moveLastLocation()
+    }
+
+    private fun moveLastLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(), Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestLocationPermission()
+            return
+        }
+
+        fusedLocationProviderClient.lastLocation.addOnSuccessListener { location ->
+            val myLocation = LatLng(location.latitude, location.longitude)
+            val marker = Marker()
+            marker.position = myLocation
+
+            marker.map = naverMap
+            val cameraUpdate = CameraUpdate.scrollTo(myLocation)
+            naverMap.moveCamera(cameraUpdate)
+            naverMap.maxZoom = 18.0
+            naverMap.minZoom = 5.0
+        }
+    }
 }
